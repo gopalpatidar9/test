4c99ea449c603ea91da65165624cab2fdeb6e73a
gopal patidar <gopalpatidara9@gmail.com.com>
2022-07-28 14:43:48 +0530

    changes for heroku

--- app/controllers/articals_controller.rb
+++ app/controllers/articals_controller.rb
@@ -1,2 +1,57 @@
 class ArticalsController < ApplicationController
+  before_action :set_artical, only: [:show, :edit, :update, :destroy ]
+
+  def show 
+  end
+
+  def index 
+    @articals = Artical.all
+  end
+
+  def new
+    @artical = Artical.new
+  end
+
+  def create
+    @artical = Artical.new(artical_params)
+    if @artical.save
+      flash[:notice] = "sucsses."
+     redirect_to artical_path(@artical)
+    else 
+      render 'new'
+    end
+  end
+
+  def edit 
+  end
+
+  def update 
+    if @artical.update(artical_params)
+      flash[:notice] = "artical updated "
+      redirect_to @artical
+    else
+      render "edit"
+    end
+
+  end
+
+  def destroy
+    @artical.destroy
+      flash[:success] = 'Artical was successfully deleted.'
+      redirect_to articals_path
+    
+  end
+
+  private
+
+  def set_artical
+    @artical = Artical.find(params[:id])
+  end
+
+  def artical_params 
+    params.require(:artical).permit(:title , :discription)
+  end
+  
+  
+  
 end
